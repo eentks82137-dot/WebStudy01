75e9993a67b6c3e30c3dd8e7aa888751ded816b7
eentks82137-dot <eentks82137@gmail.com>
2026-04-09 14:04:50 +0900

    System.out.print -> log.info

--- src/main/java/kr/or/ddit/db/ConnectionFactory.java
+++ src/main/java/kr/or/ddit/db/ConnectionFactory.java
@@ -5,13 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class ConnectionFactory {
     static String url;
     static String username;
     static String password;
 
     static {
-        System.out.println("======================= Before All =======================");
+        log.info("======================= ConnectionFactory Static Block =======================");
         String basename = "kr.or.ddit.db.DbInfo";
         ResourceBundle resourceBundle = ResourceBundle.getBundle(basename);
         url = resourceBundle.getString("url");
@@ -21,6 +24,7 @@ public class ConnectionFactory {
 
         try {
             Class.forName(driverClassName);
+            log.info("Driver Loading Success!!");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
